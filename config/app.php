1577156a1ea23e680275d8ea5c9c2e7970eae30f
Jadith <jmolinaresnavarro@gmail.com>
2025-06-09 08:16:44 -0500

    Volver a incluir la carpeta config

---
+++ config/app.php
@@ -0,0 +1,17 @@
+<?php
+
+
+// const APP_URL = "http://localhost/Jadith/EasyDataManagement/";
+
+//En casa de Ali
+//const APP_URL = "http://localhost/EDM/EDM/";
+
+// ADRIAN 
+// const APP_URL = "http://localhost/EasyDataManagement_2/EDM/";
+
+//En el sena
+// const APP_URL = "http://localhost/Easy%20Data%20Management/";
+const APP_NAME = "Tienda la Mano de Dios";
+const APP_SESSION_NAME = "EDM";
+
+date_default_timezone_set("America/Bogota");
