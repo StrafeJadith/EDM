1cb528de65b11ae983765d9bb2093c4a58539eff
Jadith Molinares <jmolinaresnavarro@gmail.com>
2025-06-17 11:04:38 -0500

    Arregle y verifique algunos errores de Ali

--- app/ajax/paymentMethodsAjax.php
+++ app/ajax/paymentMethodsAjax.php
@@ -1,27 +1,19 @@
 <?php
 
-require_once "../../config/app.php";
-require_once "../view/inc/session_start.php";
-require_once "../../autoload.php";
+    require_once "../../config/app.php";
+    require_once "../view/inc/session_start.php";
+    require_once "../../autoload.php";
 
-use app\controller\userPaymentController;
+    use app\controller\userPaymentController;
 
-if (isset($POST["modulo_pago"])) {
+    if (isset($_POST['modulo_pago'])) {
 
-    $insPaymentMethod = new userPaymentController();
+        $insPaymentMethod = new userPaymentController();
 
-    if ($_POST["modulo_pago"] == "pago_credito") {
+        if($_POST["modulo_pago"] == "pago_credito") {
 
-        echo $insPaymentMethod->pagoCreditoController();
+            echo $insPaymentMethod->pagoCreditoController();
+        }
+
     }
 
-}
-
-
-
-
-
-
-
-
-?>
